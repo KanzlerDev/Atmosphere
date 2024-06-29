6b40a5e12b6a331f2f361a9471320e4a37f878c5
KanzlerDev <77540089+KanzlerDev@users.noreply.github.com>
2024-06-29 10:50:33 +0300

    Update boot_charger_driver.hpp
    Default Charge Voltage Limit set to 4320.

--- stratosphere/boot/source/boot_charger_driver.hpp
+++ stratosphere/boot/source/boot_charger_driver.hpp
@@ -52,6 +52,9 @@ namespace ams::boot {
                 /* Set boost mode current limit to 500 ma. */
                 R_TRY(powctl::SetChargerBoostModeCurrentLimit(m_charger_session, 500));
 
+                /* Set Charger voltage limit to 4320 mV. */
+                R_TRY(powctl::SetChargerChargeVoltageLimit(m_charger_session, 4320));
+                
                 /* Disable hi-z mode. */
                 R_TRY(powctl::SetChargerHiZEnabled(m_charger_session, false));
 
@@ -90,7 +93,7 @@ namespace ams::boot {
             }
 
             Result SetChargeVoltageLimit(int limit) {
-                R_RETURN(powctl::SetChargerChargeVoltageLimit(m_charger_session, limit));
+                R_RETURN(powctl::SetChargerChargeVoltageLimit(m_charger_session, 4320));
             }
 
             Result GetChargerStatus(boot::ChargerStatus *out) {
