77a7b830a37f80ed28b4c340b4dae43dd9644164
KanzlerDev <77540089+KanzlerDev@users.noreply.github.com>
2024-07-11 10:12:38 +0300

    Update powctl_charger_parameters.board.nintendo_nx.cpp

--- libraries/libstratosphere/source/powctl/driver/impl/powctl_charger_parameters.board.nintendo_nx.cpp
+++ libraries/libstratosphere/source/powctl/driver/impl/powctl_charger_parameters.board.nintendo_nx.cpp
@@ -38,20 +38,6 @@ namespace ams::powctl::driver::impl {
         constexpr inline const float FloatMax = std::numeric_limits<float>::max();
         constexpr inline const float FloatMin = -FloatMax;
 
-        constexpr inline const UnknownParameterX UnknownXTableForBatteryVersion2[] = {
-            {  20000, 4320, 95.0, 100.4 },
-            {  30000, 4304, 94.0,  99.7 },
-            {  40000, 4288, 93.0,  98.4 },
-            {  50000, 4272, 92.0,  97.0 },
-            {  60000, 4256, 90.0,  95.7 },
-            {  80000, 4240, 89.0,  94.2 },
-            { 100000, 4224, 88.0,  93.0 },
-            {    Max, 4192, 85.0,  90.0 },
-        };
-
-        /* Include automatically extracted charger parameters. */
-        #include "powctl_charger_parameters.board.nintendo_nx.inc"
-
     }
 
     const ChargeParameters &GetChargeParameters() {
